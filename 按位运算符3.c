e71e23f2ce8dbaafe785a30f76d707de0207e138
beyondlife <813106093@qq.com>
2014-08-19 00:29:05 +0800

    按位运算test

--- 按位运算符3.c
+++ 按位运算符3.c
@@ -1,3 +1,26 @@
 // 编写函数rightrot(x,n),该函数返回将x循环右移n位后所得到的值
 
+//计算字长
+int getIntBit(){
+	unsigned tmp = (unsigned)~0;
+	int result = 0;
+	do{
+		result++;
+	}while(tmp=(tmp<<1));
+	return result;
+}
 
+int rightrot(int x,int n){
+	int tmp = x;
+
+	return ((~(~0 << n) & 1) & x) << (getIntBit() - n)
+	 	   |  (tmp >> n);
+}
+
+int main(void){
+	int a = 0x60;
+	int result = rightrot(a,4);
+	printf("%x\n",result);
+	getchar();
+	return 0;
+}
